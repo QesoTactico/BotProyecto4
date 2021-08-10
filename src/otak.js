7f9d558afc627d01d540ad454f4e44322ae228e5
QesoTactico <88404290+QesoTactico@users.noreply.github.com>
2021-08-10 17:08:58 -0500

    Update otak.js

--- src/otak.js
+++ src/otak.js
@@ -1,17 +1,17 @@
 const otak = (prefix, pushname) => {
-    return `*Palabras especificas para que el bot interactue con ustedes mis queridos otakus*
+    return `*Palabras especificas para que el bot interactue con ustedes Licenciados otakus*
 
 
 Quien es tu sempai botsito
 Me gimes 7u7
 Te amo botsito uwu
 Onichan
-La toca 7w7
+La toca 
 
 
 _*Ojito escribe tal como esta*_
 
-ву ѕнαη∂υу`
+`
 
 }
 
