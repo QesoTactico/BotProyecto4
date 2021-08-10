1df5822ab555645cd157e46505478599e8be1471
QesoTactico <88404290+QesoTactico@users.noreply.github.com>
2021-08-10 16:25:13 -0500

    Update desmenu.js

--- src/desmenu.js
+++ src/desmenu.js
@@ -23,9 +23,9 @@ ${prefix}play https://www.youtube.com/xxxxxxxxxxxxx
 
 ══════════════
     
-_*play y *play2 tiene un limite de 2000 canciones en el server, si llega a su limite se restablecera dentro de 24 horas_
+_El *play y *play2 tiene un limite de 2000 canciones en el server, si llega a su limite se restablecera dentro de 24 horas_
      
-ву ѕнαη∂υу`
+Auque también se daña la wea esta jeje`
 
 }
 
